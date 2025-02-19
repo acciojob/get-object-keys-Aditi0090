6adda6787d55c5620cdea22d6ebe25116c6266c1
Aditi0090 <165465425+Aditi0090@users.noreply.github.com>
2025-02-19 16:20:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const student = {
+  name: "John",
+};
+
+
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Do not change code below this line
+window.student = student;
+window.getKeys = getKeys;
